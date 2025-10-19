591db932694ed2f976906a110e3a59c00151a4c4
Arthur <relentlessviper10@gmail.com>
2025-10-19 16:41:17 +0300

    Finish proper module reloading logic

--- experiments/base_fomaml.py
+++ experiments/base_fomaml.py
@@ -7,15 +7,11 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-# Only use next line to reload custom modules
-del sys.modules["rl_models"]
-reload(sys.modules["rl_models"])
-reload(sys.modules["rl_models.models"])
-reload(sys.modules["rl_models.models.fomaml"])
-
 from rl_models import FOMAML
+from utils import reload_modules
 
 if __name__ == "__main__":
     test_tensor = torch.tensor([1, 2, 3, 4, 5], dtype=torch.float32)
     model = FOMAML()
-    output = model(test_tensor)
+    output = model(test_tensor.unsqueeze(0))
+    print(output)
